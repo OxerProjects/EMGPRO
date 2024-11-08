5a9af5b6381e4d7796abd8a405eeed84e149dec3
OxerProjects <122890182+OxerProjects@users.noreply.github.com>
2024-11-08 15:13:58 +0200

    Update media.js

--- javaScript/media.js
+++ javaScript/media.js
@@ -16,7 +16,7 @@ const videoChanel = document.getElementById('videoChanel')
 const news12 = `<iframe scrolling="no" class="news" width="100%" height="100%" src="https://www.mako.co.il/AjaxPage?jspName=embedHTML5video.jsp&galleryChannelId=3bf5c3a8e967f510VgnVCM2000002a0c10acRCRD&videoChannelId=8bf955222beab610VgnVCM100000700a10acRCRD&vcmid=1e2258089b67f510VgnVCM2000002a0c10acRCRD"frameborder="0" allowfullscreen id="mainScreen" autoplay>`;
 const news11 = `<iframe class="news" width="100%" height="100%" src="https://www.kan.org.il/live/"frameborder="0" allowfullscreen id="mainScreen" autoplay>`;
 const news13 = `<iframe class="news" width="100%" height="100%" src="https://13tv.co.il/live/"frameborder="0" allowfullscreen id="mainScreen" autoplay>`;
-const news14 = `<iframe class="news" width="100%" height="100%" src="https://www.now14.co.il/live"frameborder="0" allowfullscreen id="mainScreen" autoplay>`;
+const news14 = `<iframe class="news" width="100%" height="100%" src="https://gurutv.online/ch14.html#google_vignette"frameborder="0" allowfullscreen id="mainScreen" autoplay>`;
 
 pickChanels.addEventListener("click", () => {
     chanels.className = "chanels";
@@ -44,26 +44,60 @@ chanel14.addEventListener("click", () => {
 
 //side:
 
-const one = `https://www.tzevaadom.co.il/`;
-const two = `https://hamal.co.il/main`;
+const one = `<iframe class="alertsUp" height="100%" width="100%" src="https://www.tzevaadom.co.il/"></iframe>`;
+const two = `<iframe class="alertsUp" height="100%" width="100%" src="https://hamal.co.il/main"></iframe>`;
 
 const oneAlerts = document.getElementById('oneAlerts')
 const twoAlerts = document.getElementById('twoAlerts')
 const titleAlert = document.getElementById('titleAlerts')
-const t3Alerts = document.getElementById('t3Alerts')
-
 
 oneAlerts.addEventListener("click", () => {
-    alerts.src = one;
+    alerts.innerHTML = one;
     titleAlert.innerHTML = "התראות צופר"
 })
 
 twoAlerts.addEventListener("click", () => {
-    alerts.src = two;
+    alerts.innerHTML = two;
     titleAlert.innerHTML = "מערכת חמל"
 })
 
+//{title: "בית דגן", time: "19:11"}, {title: "בית דגן", time: "13:12"}
+
+let Alerts = [];
+
+const t3Alerts = document.getElementById('t3Alerts')
+let alertsos;
+
+
 t3Alerts.addEventListener("click", () => {
-    alerts.src = "";
+    alertsos = `<h1 style="color: white; text-align: center;" dir="rtl">התרעות אחרונות:</h1>`
+    Alerts.forEach(alert => {
+        alertsos += `<div class="alt">
+                        <span class="altTitle" style="color: white">${alert.time}</span>
+                        <span class="altTitle" style="color: white">${alert.title}</span>
+                    </div>`
+    });
+    alerts.innerHTML = alertsos;
     titleAlert.innerHTML = "התראות באזורך"
+})
+
+const height = document.getElementById('height')
+const width = document.getElementById('width')
+const resetDesign = document.getElementById('resetDesign')
+
+
+width.addEventListener("input", () => {
+    document.body.style.paddingLeft = width.value + "rem";
+    document.body.style.paddingRight = width.value + "rem";
+})
+
+height.addEventListener("input", () => {
+    document.body.style.paddingTop = height.value + "rem";
+    document.body.style.paddingBottom = height.value + "rem";
+})
+
+resetDesign.addEventListener("click", () => {
+    document.body.style.height = "100vh";
+    document.body.style.padding = "1rem";
+
 })
